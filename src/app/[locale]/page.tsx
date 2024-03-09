75d5f578f6dfb617ed0eedf72f00ead42b1ad57d
testsnake <emma@meredithblack.com>
2024-03-08 18:04:06 -0800

    Update page.tsx

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -7,7 +7,7 @@ export default function HomePage() {
     return (
         <div className="flex h-screen dark:">
             <div className="mx-auto mt-40 md:mt-[20vh] ">
-                <div className="align-top w-[90vw] md:w-[340px]">
+                <div className="align-top w-[90vw] md:w-[320px]">
                     <AspectRatio ratio={3 / 2}>
                         <InfoCard />
                     </AspectRatio>
