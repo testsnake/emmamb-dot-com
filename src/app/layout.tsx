144a1ab70c2f64c26447a1e0e5e4da9a56c14792
testsnake <emma@meredithblack.com>
2024-03-12 18:24:16 -0700

    Fix: update i18n strings

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,3 @@
-export const metadata = {
-    title: 'Emma Meredith-Black',
-    description: '',
-    icons: [{ rel: 'icon', url: '/favicon.ico' }]
-};
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html>
