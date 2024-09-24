8d477ef82b7200101db67485d021d0d31d642723
Emma MB <emma@meredithblack.com>
2024-09-23 19:40:06 -0700

    Remove EVERYTHING

--- src/navigation.ts
+++
@@ -1,7 +0,0 @@
-import { createSharedPathnamesNavigation } from 'next-intl/navigation';
-import supported from './i18nAvalibleLocales.json';
-
-export const locales = supported.locales;
-export const localePrefix = 'as-needed';
-
-export const { Link, redirect, usePathname, useRouter } = createSharedPathnamesNavigation({ locales, localePrefix });
