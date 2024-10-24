60654fa0d67dce6caa55d09cd328ac3b4223e325
Emma MB <emma@meredithblack.com>
2024-10-23 17:35:19 -0700

    Revert "Bridgy fed support"
    This reverts commit ae6d532852b000570a41aa93310f15a159732ab3.

--- src/app/.well-known/host-meta/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export async function GET(request: Request) {
-    const { searchParams } = new URL(request.url);
-    const query = searchParams.toString();
-
-    return NextResponse.redirect(`https://fed.brid.gy/.well-known/host-meta?${query}`, 302);
-}
