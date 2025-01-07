dff24ae156be4e8d53e57a585962215596366f37
Emma MB <emma@meredithblack.com>
2025-01-07 13:52:32 -0800

    Update info-card.tsx

--- src/components/info-card.tsx
+++ src/components/info-card.tsx
@@ -51,9 +51,6 @@ const InfoCard = () => {
                 <ButtonLink href="https://www.linkedin.com/in/emma-meredith-black/" name="linkedin">
                     <LinkedIn />
                 </ButtonLink>
-                <ButtonLink href="https://bsky.app/profile/emmamb.com" name="Bluesky">
-                    <Bluesky />
-                </ButtonLink>
                 <ButtonLink href="https://www.instagram.com/emma0mb/" name="Instagram">
                     <Instagram />
                 </ButtonLink>
